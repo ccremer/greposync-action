62afed88f5b517df68667560efbf017fe8c222db
Chris <github.account@chrigel.net>
2021-07-05 20:44:08 +0200

    Update GH script to v4

--- .github/move-tag.js
+++ .github/move-tag.js
@@ -2,7 +2,7 @@ module.exports = ({ github, context }) => {
   const tag = context.ref.substring(10, 12)
   console.log("tag: " + tag)
   try {
-    github.git.deleteRef({
+    await github.git.deleteRef({
       owner: context.repo.owner,
       repo: context.repo.repo,
       ref: "tags/" + tag
@@ -10,7 +10,7 @@ module.exports = ({ github, context }) => {
   } catch (e) {
     console.log("The tag '" + tag + "' doesn't exist yet: " + e)
   }
-  github.git.createRef({
+  await github.git.createRef({
     owner: context.repo.owner,
     repo: context.repo.repo,
     ref: "refs/tags/" + tag,
