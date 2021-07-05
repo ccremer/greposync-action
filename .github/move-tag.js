bac1524a833b8e2aa95636a7566f09a8a9020ce7
Chris <github.account@chrigel.net>
2021-07-05 20:31:43 +0200

    Don't run asynchronously

--- .github/move-tag.js
+++ .github/move-tag.js
@@ -2,7 +2,7 @@ module.exports = ({ github, context }) => {
   const tag = context.ref.substring(11, 13)
   console.log("tag: " + tag)
   try {
-    await github.git.deleteRef({
+    github.git.deleteRef({
       owner: context.repo.owner,
       repo: context.repo.repo,
       ref: "tags/" + tag
@@ -10,7 +10,7 @@ module.exports = ({ github, context }) => {
   } catch (e) {
     console.log("The tag '" + tag + "' doesn't exist yet: " + e)
   }
-  await github.git.createRef({
+  github.git.createRef({
     owner: context.repo.owner,
     repo: context.repo.repo,
     ref: "refs/tags/" + tag,
